0a803493916ba565273ca5ed01e66a9d72d16a90
Phillips <elena.phillips@fscj.edu>
2023-11-15 16:40:46 -0500

    Java 1 class

---
+++ m8/edu/fscj/cop2800c/statistics/StatisticsCalculator.java
@@ -0,0 +1,13 @@
+package edu.fscj.cop2800c.statistics;
+
+// interface for statistical calculations
+public interface StatisticsCalculator {
+
+    // returns max from array of Double
+    public Double max(Double[] inArray) throws ArrayIndexOutOfBoundsException;
+
+    // returns mean from array of Double
+    public Double mean(Double[] inArray) throws ArrayIndexOutOfBoundsException;;
+    
+    public Double stddev(Double[] inArray) throws ArrayIndexOutOfBoundsException;
+}
